b3e4d64a94f3d31fddc57bf5289a9702994e4e19
Seungjoon <50124461+seungjoonH@users.noreply.github.com>
2026-04-27 22:30:55 +0900

    Add BOJ 1001 testcase (#1)

---
+++ testcase/boj/1001/validator.cpp
@@ -0,0 +1,11 @@
+#include "testlib.h"
+
+int main(int argc, char* argv[]) {
+	registerValidation(argc, argv);
+	inf.readInt(1, 9, "a");
+	inf.readSpace();
+	inf.readInt(1, 9, "b");
+	inf.readEoln();
+	inf.readEof();
+	return 0;
+}
