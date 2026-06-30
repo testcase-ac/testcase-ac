2585d607146cae1217da922c06be55233291041a
Jaechan <dlwocks31@gmail.com>
2026-06-30 09:24:12 +0900

    Fix BOJ 2237 checker self-validation

--- testcase/boj/2237/checker.cpp
+++ testcase/boj/2237/checker.cpp
@@ -1,57 +1,54 @@
 #include "testlib.h"
 #include <vector>
-#include <string>
 using namespace std;
 
-int main(int argc, char * argv[]) {
-    registerTestlibCmd(argc, argv);
+int N, T;
+vector<long long> seq;
 
-    // Read input
-    int N = inf.readInt();
-    int T = inf.readInt();
-    vector<long long> seq(N);
-    for (int i = 0; i < N; i++) {
-        seq[i] = inf.readInt();
-    }
-
-    // Read contestant's operations
+void readAns(InStream& stream) {
     vector<int> ops;
     for (int k = 0; k < N - 1; k++) {
-        if (ouf.seekEof()) {
-            quitf(_wa, "too few operations: expected %d, found %d", N - 1, k);
+        if (stream.seekEof()) {
+            stream.quitf(_wa, "too few operations: expected %d, found %d", N - 1, k);
         }
-        int x = ouf.readInt();
+        int x = stream.readInt();
         ops.push_back(x);
     }
-    // Check for extra tokens
-    if (!ouf.seekEof()) {
-        string extra = ouf.readToken();
-        quitf(_wa, "extra token in output: '%s'", extra.c_str());
+    if (!stream.seekEof()) {
+        stream.quitf(_wa, "extra output after %d operation(s)", N - 1);
     }
 
-    // Simulate the reduction operations
     vector<long long> v = seq;
     for (int step = 0; step < (int)ops.size(); step++) {
         int idx = ops[step];
         int len = (int)v.size();
         if (idx < 1 || idx > len - 1) {
-            quitf(_wa, "operation %d: index %d out of range [1, %d]", step + 1, idx, len - 1);
+            stream.quitf(_wa, "operation %d: index %d out of range [1, %d]", step + 1, idx, len - 1);
         }
-        // perform CON(v, idx): v[idx-1] = v[idx-1] - v[idx], then remove v[idx]
         v[idx - 1] = v[idx - 1] - v[idx];
         v.erase(v.begin() + idx);
     }
 
-    // After N-1 ops, must have exactly one number
     if ((int)v.size() != 1) {
         quitf(_fail, "internal error: sequence length is %d after %d operations, expected 1",
               (int)v.size(), N - 1);
     }
-
-    // Check final value
     if (v[0] != T) {
-        quitf(_wa, "final value is %lld, expected %d", v[0], T);
+        stream.quitf(_wa, "final value is %lld, expected %d", v[0], T);
+    }
+}
+
+int main(int argc, char * argv[]) {
+    registerTestlibCmd(argc, argv);
+
+    N = inf.readInt();
+    T = inf.readInt();
+    seq.resize(N);
+    for (int i = 0; i < N; i++) {
+        seq[i] = inf.readInt();
     }
 
+    readAns(ans);
+    readAns(ouf);
     quitf(_ok, "correct");
 }
