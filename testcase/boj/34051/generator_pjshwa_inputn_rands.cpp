bcb7a3b7ec310e2c9da41b8833816cbabc73109b
Jaechan <dlwocks31@gmail.com>
2026-06-30 21:25:55 +0900

    Fix deterministic BOJ generator fixtures

--- testcase/boj/34051/generator_pjshwa_inputn_rands.cpp
+++ testcase/boj/34051/generator_pjshwa_inputn_rands.cpp
@@ -7,7 +7,7 @@ using ll = long long;
 int main(int argc, char* argv[]) {
   registerGen(argc, argv, 1);
 
-  int N = atoi(argv[1]);
+  int N = rnd.next(1, 5000);
   cout << N << '\n';
 
   for (int i = 0; i < N; ++i) cout << (char)(rnd.next(0, 25) + 'a');
